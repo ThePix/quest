2768b3f80728d98eef961af6e1a6b62309512293
ThePix <the_pix@hotmail.com>
2017-07-21 11:17:30 +0100

    Added object prefixes

--- YamlToQuest.rb
+++ YamlToQuest.rb
@@ -1,3 +1,17 @@
+# A way to convert YAML to Quest XML
+#
+# Version 2: added scenery objects and zones
+# Version 3: added object prefixes
+#
+# By The Pixie
+
+
+# This will be added to every object and room
+# Useful for a group project to avoid name collisions
+OBJECT_PREFIX = ""
+
+
+
 require 'yaml'
 
 LIST1 = "North;Northeast;East;Southeast;South;Southwest;West;NorthWest;Up;Down;In;Out".split ";"
@@ -21,7 +35,7 @@ class Room
   end
 
   def to_s
-    s = "    <object name=\"#{@name}\">\n"
+    s = "    <object name=\"#{OBJECT_PREFIX}#{@name}\">\n"
     s += "      <inherit name=\"editor_room\" />\n"
     s += "      <alias>#{@alias}</alias>\n"
     s += "      <description><![CDATA["
@@ -29,7 +43,7 @@ class Room
     s += "]]></description>\n"
     s += "      <inside />\n" if @inside
     @objects.each do |k, v|
-      s += "      <object name=\"#{k.gsub(' ', '_').gsub(/[^\w]/, '').downcase}\">\n"
+      s += "      <object name=\"#{OBJECT_PREFIX}#{k.gsub(' ', '_').gsub(/[^\w]/, '').downcase}\">\n"
       s += "        <inherit name=\"editor_object\" />\n"
       s += "        <alias>#{k}</alias>\n"
       s += "        <scenery/>\n"
@@ -59,7 +73,7 @@ class Exit
   end
   
   def to_s
-    s = "    <exit alias=\"#{@dir.downcase}\" to=\"#{@to.name}\">\n"
+    s = "    <exit alias=\"#{@dir.downcase}\" to=\"#{OBJECT_PREFIX}#{@to.name}\">\n"
     s += "      <inherit name=\"#{@dir.downcase}direction\" />\n"
     s += "      <message>#{@message}</message>\n" if @message
     s += "    </exit>\n"
@@ -86,7 +100,7 @@ end
 
 
 
-data = YAML.load_file("town.yml")
+data = YAML.load_file("quest.yml")
 list = []
 data.each do |h|
   if h.is_a? Hash
@@ -126,7 +140,7 @@ File.open("quest.xml", "w") do |file|
       file << el.to_s
     else
       file << "  </object>\n" if flag
-      file << "  <object name=\"#{el}\">\n"
+      file << "  <object name=\"#{OBJECT_PREFIX}#{el.gsub(' ', '_').gsub(/[^\w]/, '').downcase}\">\n"
       file << "    <inherit name=\"editor_room\" />\n"
       flag = true
     end
